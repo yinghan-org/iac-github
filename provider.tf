4f6493d5c503e6ddccbbfc583d4a3014b1f469c7
yinghan <yinghan.yhtan@gmail.com>
2022-11-08 10:54:43 +0800

    fix: downgrade to 0.12.23 to match docker image

--- provider.tf
+++ provider.tf
@@ -3,7 +3,7 @@
 #######################################################################
 
 terraform {
-  required_version = "~> 0.12.24"
+  required_version = "~> 0.12.23"
   backend "s3" {
     bucket         = "github-terraform-example-terraform-state"
     key            = "organization/github-terraform-example/terraform.tfstate"
@@ -23,3 +23,19 @@ provider "github" {
 provider "aws" {
   region = "ap-southeast-1"
 }
+
+
+# terraform {
+#   required_version = "~> 0.12.9"
+#   backend "s3" {
+#     bucket         = "github-terraform-example-terraform-state"
+#     key            = "organization/github-terraform-example/terraform.tfstate"
+#     region         = "ap-southeast-1"
+#     encrypt        = true
+#     dynamodb_table = "terraform-state-lock"
+#   }
+#   required_providers {
+#     github = "~> 2.9.2"
+#     aws    = "~> 3.37"
+#   }
+# }
